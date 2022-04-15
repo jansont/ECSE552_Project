ab23bb68c265e69fef2b1add63a507e70848b77b
Ding Ma <ding.ma@mail.mcgill.ca>
2022-04-15 01:43:42 -0400

    cant get it to work on samples

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -9,16 +9,17 @@ from graph import Graph
 
 
 def get_date_range(file):
-    if file == 'LA_DATA_2018_02_to_2018_06.csv': 
-        start = date(2018,2,1)
-        end = date(2018,6,8)
-        date_range = pd.date_range(start,end-timedelta(days=1))
+    if file == 'LA_DATA_2018_02_to_2018_06.csv':
+        start = date(2018, 2, 1)
+        end = date(2018, 6, 8)
+        date_range = pd.date_range(start, end-timedelta(days=1))
         date_range = [str(x)[:10] for x in date_range]
-        return  date_range
+        return date_range
     else:
         print('wrong file name.')
 
-def data_to_numpy(weather_data, edge_cols, node_cols, pseudo_data = False, save=True): 
+
+def data_to_numpy(weather_data, edge_cols, node_cols, pseudo_data=False, save=True):
     '''Converts pandas whether data to np array. 
     Args: 
         weather_data :: pd.DataFrame
@@ -32,74 +33,87 @@ def data_to_numpy(weather_data, edge_cols, node_cols, pseudo_data = False, save=
         date_range :: list [str]
             List of dates to select from weather data. 
     '''
-    if pseudo_data: 
+    if pseudo_data:
         checkpt = 'pseudo_checkpt'
-    else: 
+    else:
         checkpt = 'checkpt'
-        
-    if not os.path.exists(checkpt):
+
+    if not save or not os.path.exists(checkpt):
         print('Checkpt doesnt exist, making it')
-        os.makedirs(checkpt)
+        if save:
+            os.makedirs(checkpt)
 
         stations = weather_data['STATION'].unique()
         stations.sort()
 
-        date_range = sorted(weather_data['DATE'].unique(), key=lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
+        date_range = sorted(weather_data['DATE'].unique(
+        ), key=lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'))
 
-        graph_node_features = np.empty((len(date_range), len(stations), len(node_cols)))
-        graph_edge_features = np.empty((len(date_range), len(stations), len(edge_cols)))
+        graph_node_features = np.empty(
+            (len(date_range), len(stations), len(node_cols)))
+        graph_edge_features = np.empty(
+            (len(date_range), len(stations), len(edge_cols)))
         graph_labels = np.empty((len(date_range), len(stations)))
         stations.sort()
 
-        for day_idx in range(len(date_range)): 
-            for station_idx in range(len(stations)): 
-                    #crop dataframe to desired date and station  
-                    vals = weather_data[weather_data['DATE'] == date_range[day_idx] ]  
-                    vals = vals[vals['STATION'] == stations[station_idx] ]         
-                    pm = vals['pm25'].values   #get pm
-                    #crop out edge features 
-                    edge_vals = vals[edge_cols]
-                    edge_vals = np.array(edge_vals.values.tolist()).flatten()  
-                    #crop out node features
-                    node_vals = vals[node_cols]
-                    node_vals = np.array(node_vals.values.tolist()).flatten() #node features as array
+        for day_idx in range(len(date_range)):
+            for station_idx in range(len(stations)):
+                # crop dataframe to desired date and station
+                vals = weather_data[weather_data['DATE']
+                                    == date_range[day_idx]]
+                vals = vals[vals['STATION'] == stations[station_idx]]
+                pm = vals['pm25'].values  # get pm
+                # crop out edge features
+                edge_vals = vals[edge_cols]
+                edge_vals = np.array(edge_vals.values.tolist()).flatten()
+                # crop out node features
+                node_vals = vals[node_cols]
+                # node features as array
+                node_vals = np.array(node_vals.values.tolist()).flatten()
 
-                    #certain stations have missing data on a given day, fill with geo mean
-                    if len(node_vals) == 0:                           
-                        node_vals = weather_data[weather_data['DATE'] == date_range[day_idx]][node_cols].mean().values
+                # certain stations have missing data on a given day, fill with geo mean
+                if len(node_vals) == 0:
+                    node_vals = weather_data[weather_data['DATE'] ==
+                                             date_range[day_idx]][node_cols].mean().values
 
-                    if len(pm) == 0:     
-                        pm = weather_data[weather_data['DATE'] == date_range[day_idx]]['pm25'].mean()
-    
-                    if len(edge_vals) == 0: 
-                        edge_vals = weather_data[weather_data['DATE'] == date_range[day_idx]][edge_cols].mean().values
+                if len(pm) == 0:
+                    pm = weather_data[weather_data['DATE'] ==
+                                      date_range[day_idx]]['pm25'].mean()
 
-                    graph_labels[day_idx, station_idx] = pm
-                    graph_node_features[day_idx, station_idx] = node_vals 
-                    graph_edge_features[day_idx, station_idx] = edge_vals
+                if len(edge_vals) == 0:
+                    edge_vals = weather_data[weather_data['DATE'] ==
+                                             date_range[day_idx]][edge_cols].mean().values
+
+                graph_labels[day_idx, station_idx] = pm
+                graph_node_features[day_idx, station_idx] = node_vals
+                graph_edge_features[day_idx, station_idx] = edge_vals
         if save:
             print('Creating checkpoint')
-            np.save(os.path.join(checkpt, 'graph_node_features'), graph_node_features)
-            np.save(os.path.join(checkpt, 'graph_edge_features'), graph_edge_features)
+            np.save(os.path.join(checkpt, 'graph_node_features'),
+                    graph_node_features)
+            np.save(os.path.join(checkpt, 'graph_edge_features'),
+                    graph_edge_features)
             np.save(os.path.join(checkpt, 'graph_labels'), graph_labels)
 
     else:
         print('Found Checkpoint, loading')
-        graph_node_features = np.load(os.path.join(checkpt, 'graph_node_features.npy') )
-        graph_edge_features = np.load(os.path.join(checkpt, 'graph_edge_features.npy') )
-        graph_labels = np.load(os.path.join(checkpt, 'graph_labels.npy') )
+        graph_node_features = np.load(
+            os.path.join(checkpt, 'graph_node_features.npy'))
+        graph_edge_features = np.load(
+            os.path.join(checkpt, 'graph_edge_features.npy'))
+        graph_labels = np.load(os.path.join(checkpt, 'graph_labels.npy'))
 
     return graph_node_features, graph_edge_features, graph_labels
 
 
-def gather_graph_data(weather_file, 
-                     edge_cols,
-                     node_cols,
-                     dist_thresh,
-                     multi_edge_feature, 
-                     use_self_loops, 
-                     pseudo_data = False,
-                     save=True):
+def gather_graph_data(weather_file,
+                      edge_cols,
+                      node_cols,
+                      dist_thresh,
+                      multi_edge_feature,
+                      use_self_loops,
+                      pseudo_data=False,
+                      save=True):
     '''
     Read csv weather, aod, pm and meta data from file. Convert to np. Create graph.
     Args: 
@@ -114,21 +128,23 @@ def gather_graph_data(weather_file,
         weather_data = pd.read_csv(weather_file)
     else:
         weather_data = pd.read_csv(path+weather_file)
-    #convert to np
-    graph_node_features, graph_edge_features, graph_labels = data_to_numpy(weather_data, edge_cols, node_cols, pseudo_data, save)
-    #build graph
-    metadata = weather_data[['STATION', 'Latitudes', 'Longitudes']].drop_duplicates()
+    stations = np.random.choice(weather_data['STATION'].unique(), 16)
+    weather_data = weather_data.loc[weather_data['STATION'].isin(stations)]
+    # convert to np
+    graph_node_features, graph_edge_features, graph_labels = data_to_numpy(
+        weather_data, edge_cols, node_cols, pseudo_data, save)
+    # build graph
+    metadata = weather_data[['STATION', 'Latitudes',
+                             'Longitudes']].drop_duplicates()
     metadata = metadata.reset_index(drop=True).sort_values('STATION')
     graph = Graph(metadata,
                   graph_edge_features,
                   dist_thresh,
-                  multi_edge_feature, 
+                  multi_edge_feature,
                   use_self_loops)
     graph_edge_features = graph.edge_attr
     graph_data = graph_edge_features, graph_node_features, graph_labels
     return graph, graph_data
-
-
 
 
 def assign_id(weather_data):
